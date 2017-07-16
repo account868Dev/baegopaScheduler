26ad2a216d941edcc5345062338b7faeedfbf4f5
highluck <yello1109@daum.net>
2017-07-16 22:00:49 +0900

    구글 api 적용중

--- src/main/java/com/high/baegopa/common/helpers/GooglePlaceHelper.java
+++ src/main/java/com/high/baegopa/common/helpers/GooglePlaceHelper.java
@@ -1,9 +1,45 @@
 package com.high.baegopa.common.helpers;
 
+import java.net.URLDecoder;
+
 /**
  * Created by high on 2017. 7. 8..
  */
 public class GooglePlaceHelper {
 
     public static final String GEO_METRY =  "http://maps.googleapis.com/maps/api/geocode/json?address=%s&language=ko&sensor=false";
+    public static final String PLACE_API_URL = "https://maps.googleapis.com/maps/api/place";
+
+
+    public static final String API_KEY = "&key=AIzaSyCssWbcXwETRcAg4OwB8-bxc0xfKWVpRzc";
+
+    public static String getNearsearch(Double lat, Double lng){
+        String url = PLACE_API_URL + "/nearbysearch/json?location="
+                + lat + "," + lng + "&radius=1000&types=food" + API_KEY;
+        try {
+            return URLDecoder.decode(url, "UTF-8");
+        } catch (Exception e) {
+            return url;
+        }
+    }
+
+    public static String getDetail(String placeId){
+        String url = PLACE_API_URL +"/details/json?placeid="
+                + placeId + API_KEY;
+        try {
+            return URLDecoder.decode(url, "UTF-8");
+        } catch (Exception e) {
+            return url;
+        }
+    }
+
+    public static String getPlaceImage(String imgRef){
+        String url = PLACE_API_URL + "/photo?maxwidth=400&photoreference="
+                + imgRef + API_KEY;
+        try {
+            return URLDecoder.decode(url, "UTF-8");
+        } catch (Exception e) {
+            return url;
+        }
+    }
 }
--- src/main/java/com/high/baegopa/jobs/PlaceApiJob.java
+++ src/main/java/com/high/baegopa/jobs/PlaceApiJob.java
@@ -1,14 +1,49 @@
 package com.high.baegopa.jobs;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.deser.std.ObjectArrayDeserializer;
+import com.google.common.collect.Maps;
 import com.high.baegopa.common.helpers.ApplicationJobs;
+import com.high.baegopa.common.helpers.GooglePlaceHelper;
+import com.high.baegopa.common.helpers.HttpClientProxy;
+import com.high.baegopa.common.helpers.JsonHelper;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+import java.util.Optional;
 
 /**
  * Created by high on 2017. 7. 8..
  */
+@Slf4j
 public class PlaceApiJob implements ApplicationJobs{
 
     @Override
     public void OnStart() throws Exception {
 
+    }
+
+    public void setRestauranttNearsearch(){
+        String url = GooglePlaceHelper.getNearsearch(37.566535, 126.9779692);
+
+        JsonNode placeJson = HttpClientProxy.httpRequestToJsonNode(url).get("results");
+        placeJson.forEach( place -> {
+            Map<String, Object> map = Maps.newHashMap();
+            place.get("place_id");
+            place.get("name");
+            place.get("photos").get(0).get("photo_reference");
+            place.get("price_level");
+            place.get("rating");
+            Optional.ofNullable(getLatLong(place.get("geometry")))
+                    .ifPresent( geo -> {
+                        map.put("latitude", geo.get("lat"));
+                        map.put("longtitude", geo.get("lng"));
+                    });
+        });
+    }
+
+    private Map<String, Object> getLatLong(JsonNode geo) {
+        log.info("geoMetry : " + geo);
+        return JsonHelper.jsonToMap(geo.get("location").toString());
     }
 }
